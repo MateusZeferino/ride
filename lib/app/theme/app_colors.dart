d437303424d99f0ce284eb6b053051c81c19ec75
MateusZeferino <mateuszjanuario@hotmail.com>
2026-03-19 19:45:50 -0300

    Criado o projeto Flutter

--- lib/app/theme/app_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primary = Color(0xFF153757);
-  static const Color background = Color(0xFFF4F4F4);
-  static const Color fieldBackground = Color(0xFFCED3D9);
-  static const Color white = Colors.white;
-}
--- tmp_extracted_screens/travelly_flutter/assets/fonts/README.txt
+++
@@ -1,9 +0,0 @@
-Coloque aqui os arquivos de fonte abaixo:
-
-- BebasNeue-Regular.ttf
-- Oswald-ExtraLight.ttf
-- Oswald-Regular.ttf
-- Oswald-Medium.ttf
-- Oswald-Bold.ttf
-
-Observação: os arquivos de fonte não acompanham este pacote.
